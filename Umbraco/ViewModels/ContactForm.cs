6e3e2ca59667bec287d206b573241819a096825f
lukasvcl <lva@ucommerce.net>
2019-09-22 16:34:10 +0200

    l04 progress

--- Umbraco/ViewModels/ContactForm.cs
+++ Umbraco/ViewModels/ContactForm.cs
@@ -7,5 +7,9 @@ namespace Umbraco.ViewModels
 {
     public class ContactForm
     {
+        public string Name {get; set;}
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
     }
 }
