415e7cf2a0fb0f902807558637d258b08460ca4a
lukasvcl <lva@ucommerce.net>
2019-09-22 23:54:12 +0200

    finished first 4 tabs

---
+++ Umbraco/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.ViewModels;
+using Umbraco.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace Umbraco.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+        [HttpPost]
+        public ActionResult HandleFormSubmit (ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            GuidUdi currentPageUdi = new GuidUdi(CurrentPage.ContentType.ItemType.ToString(), CurrentPage.Key);
+            IContent msg = Services.ContentService.CreateContent(model.Subject, currentPageUdi, "message");
+            msg.SetValue("messageName", model.Name);
+            msg.SetValue("email", model.Email);
+            msg.SetValue("subject", model.Subject);
+            msg.SetValue("messageContent", model.Message);
+            msg.SetValue("umbracoNaviHide", true);
+
+            TempData["success"] = true;
+            Services.ContentService.Save(msg);
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+    
+}
--- Umbraco/ViewModels/ContactForm.cs
+++ Umbraco/ViewModels/ContactForm.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Umbraco.ViewModels
 {
     public class ContactForm
     {
+        [Required( ErrorMessage ="Please provide name")]
         public string Name {get; set;}
+
+        [Required(ErrorMessage = "Please provide email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please provide subject")]
         public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please provide Message")]
         public string Message { get; set; }
     }
 }
